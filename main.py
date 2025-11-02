29d092881c7f3d57173804685bc35fb57d722e81
F-K-C <fakocarvalho@gmail.com>
2025-11-02 17:55:21 -0300

    update docker novo

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ def receive_message():
     return jsonify({"Mensagem codificada em base64": text_b64, "status": "ok"})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3030)
+    app.run(debug=True, host="0.0.0.0", port=3030)
